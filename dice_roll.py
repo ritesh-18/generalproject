ba21d27d977c9826dec2ce71d0ec6bd4e19eda02
Ritesh chauhan <98571772+ritesh-18@users.noreply.github.com>
2022-05-22 01:54:31 +0530

    Update dice_roll.py

--- dice_roll.py
+++ dice_roll.py
@@ -1,6 +1,6 @@
 import random
-again = True
-while again:
+repeat = True
+while repeat:
     print(random.randint(1, 6))
     another_roll = input("want to roll again? (y/n): ")
     if another_roll.lower()=="y":
