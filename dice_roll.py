9ba61bb3fc475b65d6a781868213820bf2c97087
Ritesh chauhan <98571772+ritesh-18@users.noreply.github.com>
2022-05-22 01:52:59 +0530

    Add files via upload

---
+++ dice_roll.py
@@ -0,0 +1,10 @@
+import random
+again = True
+while again:
+    print(random.randint(1, 6))
+    another_roll = input("want to roll again? (y/n): ")
+    if another_roll.lower()=="y":
+        continue
+    else:
+        break
+        
